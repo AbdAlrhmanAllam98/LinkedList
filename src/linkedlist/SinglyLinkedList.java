625953ece0d02023ef62d890fdcf8b9ee11465fa
Abd Alrhman Allam <62191765+AbdAlrhmanAllam98@users.noreply.github.com>
2020-06-15 21:48:16 +0200

    singly linked list features

--- src/linkedlist/SinglyLinkedList.java
+++ src/linkedlist/SinglyLinkedList.java
@@ -1,14 +1,206 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package linkedlist;
+import model.Node;
+import java.lang.Math;
+public class SinglyLinkedList<T> {
+    Node head, tail;
+    int length;
 
-/**
- *
- * @author Abd Alrhman Allam
- */
-public class SinglyLinkedList {
+    public SinglyLinkedList() {
+        head = tail = null;
+        length = 0;
+    }
+
+    private boolean isEmpty() {
+        return length == 0;
+    }
+
+    //insertion
+    public void insertFirst(T element) {
+        Node newNode = new Node(element);
+        if (isEmpty()) {
+            head = tail = newNode;
+            newNode.setNext(null);
+        } else {
+            newNode.setNext(head);
+            head = newNode;
+        }
+        length++;
+    }
     
+    public void insertLast(T element) {
+        Node newNode = new Node(element);
+        if (isEmpty()) {
+            head = tail = newNode;
+            newNode.setNext(null);
+        } else {
+            tail.setNext(newNode);
+            newNode.setNext(null);
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public void insertAt(int position, T element) {
+        if (position < 0 || position > length) {
+            System.out.println("the position is not exist");
+        } else if (position == 0) {
+            insertFirst(element);
+        } else if (position == length) {
+            insertLast(element);
+
+        } else {
+            Node newNode = new Node(element);
+            Node curr = head;
+            for (int i = 1; i < position; i++) {
+                curr = curr.getNext();
+            }
+            newNode.setNext(curr.getNext());
+            curr.setNext(newNode);
+            length++;
+        }
+    }
+
+    //Deletion
+    public void removeFirst() {
+        Node curr = head;
+        if (isEmpty()) {
+            System.out.println("the list is empty");
+            return;
+        } else if (length == 1) {
+            head = tail = null;
+            length--;
+        } else {
+            head = curr.getNext();
+            length--;
+        }
+
+    }
+
+    public void removeLast() {
+        if (isEmpty()) {
+            System.out.println("the list is empty");
+        } else if (length == 1) {
+            tail = head = null;
+            length--;
+        } else {
+            Node curr = head.getNext();
+            Node prev = head;
+            while (curr != tail) {
+                prev = curr;
+                curr = curr.getNext();
+            }
+            prev.setNext(null);
+            tail = prev;
+            length--;
+        }
+    }
+
+    public void removeAt(int position) {
+        if (position < 0 || position > length) {
+            System.out.println("the position is not exist");
+            return;
+        }
+        if (isEmpty()) {
+            System.out.println("the list is empty");
+        } else if (position == 0) {
+            removeFirst();
+        } else if (position == length) {
+            removeLast();
+        } else {
+            Node curr = head.getNext();
+            Node prev = head;
+            for (int i = 1; i < position; i++) {
+                prev = curr;
+                curr = curr.getNext();
+            }
+            prev.setNext(curr.getNext());
+            length--;
+
+        }
+    }
+
+    public void remove(T element) {
+        if (isEmpty()) {
+            System.out.println("the list is empty");
+            return;
+        }
+        if (head.getItem() == element) {
+            removeFirst();
+        } else if (tail.getItem() == element) {
+            removeLast();
+        } else {
+            Node curr = head.getNext();
+            Node prev = head;
+            while (curr != null && curr.getItem() != element) {
+                prev = curr;
+                curr = curr.getNext();
+            }
+            if (curr != null) {
+                prev.setNext(curr.getNext());
+            } else if (curr == null) {
+                System.out.println("The item is not there");
+            }
+        }
+    }
+
+    public void reverse() {
+        if (isEmpty()) {
+            System.out.println("the list is empty");
+        } else {
+            Node prev = null;
+            Node curr = head;
+            Node next = curr.getNext();
+            tail = head;
+            while (next != null) {
+                next = curr.getNext();
+                curr.setNext(prev);
+                prev = curr;
+                curr = next;
+            }
+            head = prev;
+        }
+    }
+
+    public int getPosition(T element) {
+        int pos = 0;
+        Node curr = head;
+        if (head.getItem() == element) {
+            return 0;
+        } else if (tail.getItem() == element) {
+            return length-1;
+        } else {
+            while (curr != tail && curr.getItem() != element) {
+                curr = curr.getNext();
+                pos++;
+            }
+            return pos;
+
+        }
+    }
+    
+    public void middleNode() {
+        if(length==0)
+        {
+            System.out.println("the list is empty");
+            return;
+        }
+        int mid=(int) Math.ceil(length/2);
+        Node curr=head;
+        for(int i=0;i<mid;i++)
+        {
+            curr=curr.getNext();
+        }
+        while(curr!=null)
+        {
+            System.out.print(curr.getItem()+"\t");
+            curr=curr.getNext();
+        }
+    }
+    public void print() {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.getItem() + "\t");
+            curr = curr.getNext();
+        }
+    }
 }
--- src/model/Node.java
+++ src/model/Node.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-/**
- *
- * @author Abd Alrhman Allam
- */
-public class Node {
-    
+public class Node<T> {
+
+    T item;
+    Node next;
+    Node prev;
+
+    public Node(T item) {
+        this.item = item;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
